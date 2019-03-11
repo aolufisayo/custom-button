71b1da580a0f2aec019f572e0b8c39b4450ff363
Olufisayo Akinmoju <hfisk000@email.cpcc.edu>
2019-03-11 23:26:28 +0100

    added .npmignore

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import 'storybook-addon-jsx'
 import CustomButton from '../../src/components'
 
-storiesOf("custom button", module).addWithJSX("simple", () => <CustomButton />)
+storiesOf("custom button", module).addWithJSX("simple", () => (
+    <button>I am a Button</button>
+))
 
 
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 class CustomButton extends Component {
+
+
     render() {
         return (
             <button>I am a Button</button>
